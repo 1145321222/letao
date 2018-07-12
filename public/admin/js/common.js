1f10043b129d7f458e3c984bcc3a2dc984cf783b
1145321222 <1145321222@qq.com>
2018-07-12 21:12:08 +0800

    index功能完成

--- public/admin/js/common.js
+++ public/admin/js/common.js
@@ -9,3 +9,37 @@ $(document).ajaxStop(function () {
         NProgress.done();
     }, 500);
 });
+
+// 二级菜单栏隐藏 与显示 
+//点击 一级菜单 跳出二级菜单
+//通过获取 second这个类名给他的上一个兄弟元素 a 注册事件
+
+$('.second').prev().on('click', function(){
+    $(this).next().slideToggle();
+});
+
+
+
+$(".icon_menu").on("click", function () {
+    $(".lt_aside").toggleClass("active");
+    $("body").toggleClass("active");
+  });
+
+
+  //退出功能 点击图标 显示模态框
+
+$('.icon_logout').on('click', function(){
+    $('#logoutModal').modal("show");
+})
+
+$('.btn_logout').on('click', function(){
+    $.ajax({
+        type: 'get',
+        url: '/employee/employeeLogout',
+        success: function(info){
+          if(info.success) {
+            location.href = "login.html";
+          }
+        }
+    });
+})
